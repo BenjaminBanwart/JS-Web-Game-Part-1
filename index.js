bfa488e95dc8c6725b5a8c892ae463c56a18a600
Benjamin Banwart <benjaminbanwart@Benjamins-MacBook-Pro.local>
2021-12-23 13:44:12 -0600

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,49 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+//function to add images
+function newImage(source, left, bottom) {
+    let object = document.createElement('img')
+    object.src = source
+    object.style.position = 'fixed'
+    object.style.left = left
+    object.style.bottom = bottom
+    document.body.append(object)
+}
+
+//insertion of images
+newImage('assets/green-character.gif', '100px', '100px');
+
+newImage('assets/pine-tree.png', '450px', '200px');
+
+newImage('assets/tree.png', '200px', '300px');
+
+newImage('assets/crate.png', '150px', '200px');
+
+newImage('assets/pillar.png', '350px', '100px');
+
+newImage('assets/well.png', '500px', '425px');
+
+
+//function to add items and make them disapear with a double-click
+function newItem(source, left, bottom) {
+    let object = document.createElement('img')
+    object.src = source
+    object.style.position = 'fixed'
+    object.style.left = left
+    object.style.bottom = bottom
+    document.body.append(object)
+
+object.addEventListener('dblclick', function(){
+    object.remove()
+})
+}
+
+//insertion of items
+newItem('assets/sword.png', '500px', '405px');
+
+newItem('assets/sheild.png', '165px', '185px');
+
+newItem('assets/staff.png', '600px', '100px');
+
+
+
+
+
